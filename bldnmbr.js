0b225db3570698a56c0e79591fd6ff0d0de25b71
Mark Strayer <mark@markstrayer.com>
2014-11-02 11:40:37 -0500

    initial code commit

---
+++ bldnmbr.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var app = express();
+
+
+var redis = require("redis"),
+    redisclient = redis.createClient(YOUR_REDIS_INFORMATION);
+
+
+redisclient.auth(YOUR_REDIS_KEY, function (err) {
+ if (err) { throw err; }
+ // You are now connected to your redis.
+ console.log("connected to REDIS...");
+});
+
+
+
+app.use('/', express.static(__dirname + '/site'));
+
+app.use(function numberMiddleware(req, res, next) {
+  // req.url starts with "/bar"
+  var urlObj = require('url').parse(req.url, true)
+  
+  var path = urlObj.pathname.replace('/','');
+  console.log("path:" + path);
+  if (path !="index.html")
+  {
+    if (urlObj.query["setBuildNumber"])
+    {
+      redisclient.set(path,urlObj.query["setBuildNumber"]);
+    }
+    redisclient.get(path, function(err, reply){
+      if (err)
+      {
+        redisclient.set(path,"-1");
+      }
+      redisclient.incr(path);
+      redisclient.get(path, function(err, reply){
+        if (err)
+        {
+          res.writeHead(500, {'Content-Type': 'text/plain'});
+          res.end("NOT FOUND");
+        } else
+        {
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          res.end(reply);
+        }
+      });
+        
+      
+
+    });
+
+  }
+});
+app.listen(80, function() { console.log('listening')});
